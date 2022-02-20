35a8eccb3e713da546993859229c37347183738a
Vaish07-cybar <badheviashnavi6@gmail.com>
2022-02-21 02:23:40 +0530

    created a final app

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import Todo from './Todo'
 import Todoform from './TodoForm';
 
